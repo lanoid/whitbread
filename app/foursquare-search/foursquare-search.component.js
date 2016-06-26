b82e25832373d7e9e5e5b61cbf4fcc3c356ff4b7
Matthew Lane <matthew.lanoid@gmail.com>
2016-06-26 18:23:24 +0100

    Pulling response from Foursqaure

--- app/foursquare-search/foursquare-search.component.js
+++ app/foursquare-search/foursquare-search.component.js
@@ -1,6 +1,27 @@
+'use strict';
+
 angular.
     module('foursquareSearch').
     component('foursquareSearch', {
         templateUrl : 'foursquare-search/foursquare-search.template.html',
-        controller : function foursquareSearchController() {}
+        controller : ['$http', function foursquareSearch($http) {
+            var self = this;
+            self.place = '';
+            self.restaurants = {};
+
+            self.search = function search(place) {
+                console.log(place);
+                $http.get('http://api.foursquare.com/v2/venues/search', {
+                    params : {
+                        client_id : 'K44KPZIQF0R52SZMNSJD3DS2ZDF5O1UKOQ5LKG4KY1NFC4OB',
+                        client_secret : 'UXEB1N1HQERJRQBZ0MYL3WEE1NDHGJTK0YLBIAK5ZXUF44KP',
+                        v : '20160101',
+                        near : place
+
+                    }
+                }).then(function(response){
+                    console.info(response);
+                });
+            };
+        }]
     })
