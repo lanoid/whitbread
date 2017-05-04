681270bbb5aaf7648211fa593f7721863f9738a7
Matthew Lane <matthew.lanoid@gmail.com>
2017-05-04 08:03:23 +0100

    :bug: Fixing CORS and security over HTTPS :bug:

--- app/foursquare-search/foursquare-search.component.js
+++ app/foursquare-search/foursquare-search.component.js
@@ -10,7 +10,7 @@ angular.
             self.venues = {};
 
             self.search = function search(place) {
-                $http.get('http://api.foursquare.com/v2/venues/search', {
+                $http.get('https://api.foursquare.com/v2/venues/search', {
                     params : {
                         client_id : 'K44KPZIQF0R52SZMNSJD3DS2ZDF5O1UKOQ5LKG4KY1NFC4OB',
                         client_secret : 'UXEB1N1HQERJRQBZ0MYL3WEE1NDHGJTK0YLBIAK5ZXUF44KP',
