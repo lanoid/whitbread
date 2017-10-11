79244bec954d3a963bfc1dc1309b5a270472306b
Matthew Lane <916179+lanoid@users.noreply.github.com>
2017-10-11 07:52:20 +0100

    :bug: Fixing broken test using https instead of http

--- app/foursquare-search/foursqaure-search.component.spec.js
+++ app/foursquare-search/foursqaure-search.component.spec.js
@@ -6,7 +6,7 @@ describe('foursquareSearch', function () {
         var $httpBackend,
             ctrl,
             searchTerm = 'London',
-            searchURL = 'http://api.foursquare.com/v2/venues/search?client_id=K44KPZIQF0R52SZMNSJD3DS2ZDF5O1UKOQ5LKG4KY1NFC4OB&client_secret=UXEB1N1HQERJRQBZ0MYL3WEE1NDHGJTK0YLBIAK5ZXUF44KP&near=London&v=20160101',
+            searchURL = 'https://api.foursquare.com/v2/venues/search?client_id=K44KPZIQF0R52SZMNSJD3DS2ZDF5O1UKOQ5LKG4KY1NFC4OB&client_secret=UXEB1N1HQERJRQBZ0MYL3WEE1NDHGJTK0YLBIAK5ZXUF44KP&near=London&v=20160101',
             response = {
                 response : {
                     venues : [
